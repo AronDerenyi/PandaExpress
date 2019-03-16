05d3aeb7aa9bd0c7f0e2b63b9aeb2d439d9ea74a
AronDerenyi <aron.derenyi@gmail.com>
2019-03-16 21:14:31 +0100

    Added possibility to disable logger and to show, on which object the function is called

--- src/hu/bme/iit/beta/pandaexpress/debug/Logger.java
+++ src/hu/bme/iit/beta/pandaexpress/debug/Logger.java
@@ -6,41 +6,59 @@ public class Logger {
 
 	private static Map<Object, String> aliases = new HashMap<>();
 	private static Deque<String> functionStack = new LinkedList<>();
+	private static boolean enabled = true;
+
+	public static boolean isEnabled() {
+		return enabled;
+	}
+
+	public static void enable() {
+		enabled = true;
+	}
+
+	public static void disable() {
+		enabled = false;
+	}
 
 	public static <T> T addAlias(T object, String alias) {
 		aliases.put(object, alias);
 		return object;
 	}
 
-	public static void startFunction(String functionName, Object... params) {
+	public static void startFunction(Object object, String functionName, Object... params) {
 		//print tabs to follow stack tree
-		writeTabs();
+		printTabs();
 		functionStack.push(functionName);
 
-		// print "function()"
-		System.out.print(functionName + "(");
+		// print "object.function()" if object is not null
+		if (object != null) print(objectToString(object) + ".");
+		print(functionName + "(");
 
 		// if there are params, print parameters "param1, param2" etc.
 		for (Object param : params) {
-			System.out.print(objectToString(param));
+			print(objectToString(param));
 			if (param != params[params.length - 1]) {
-				System.out.print(", ");
+				print(", ");
 			}
 		}
 
-		System.out.print(") {");
+		print(") {");
 
 		// linebreak
-		System.out.println();
+		printNewLine();
+	}
+
+	public static void startFunction(String functionName, Object... params) {
+		startFunction(null, functionName, params);
 	}
 
 	public static <T> T endFunction(T returnValue) {
 		functionStack.pop();
-		writeTabs();
+		printTabs();
 
-		System.out.print("}");
-		if (returnValue != null) System.out.print(" return " + objectToString(returnValue));
-		System.out.println();
+		print("}");
+		if (returnValue != null) print(" return " + objectToString(returnValue));
+		printNewLine();
 
 		return returnValue;
 	}
@@ -54,7 +72,21 @@ public class Logger {
 		return aliases.getOrDefault(obj, obj.toString());
 	}
 
-	private static void writeTabs() {
-		functionStack.forEach((function) -> System.out.print("\t"));
+	private static void print(Object object) {
+		if (enabled) {
+			System.out.print(object);
+		}
+	}
+
+	private static void printTabs() {
+		if (enabled) {
+			functionStack.forEach((function) -> System.out.print("\t"));
+		}
+	}
+
+	private static void printNewLine() {
+		if (enabled) {
+			System.out.println();
+		}
 	}
 }
