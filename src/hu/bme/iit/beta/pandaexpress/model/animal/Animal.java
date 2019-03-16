0358178e1d96ca9cfddf5821ee112ecc618c46fd
AronDerenyi <aron.derenyi@gmail.com>
2019-03-16 16:05:36 +0100

    Create basic animal and tile classes with their functionalities.

--- src/hu/bme/iit/beta/pandaexpress/model/animal/Animal.java
+++ src/hu/bme/iit/beta/pandaexpress/model/animal/Animal.java
@@ -1,4 +1,85 @@
 package hu.bme.iit.beta.pandaexpress.model.animal;
 
+import hu.bme.iit.beta.pandaexpress.debug.Logger;
+import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
+
 public class Animal {
+
+	private boolean dead = false;
+
+	private Tile tile = null;
+
+	private Animal following = null;
+	private Animal followedBy = null;
+
+	public Animal() {
+		Logger.startFunction("Animal");
+		Logger.endFunction();
+	}
+
+	public boolean isDead() {
+		Logger.startFunction("isDead");
+		return Logger.endFunction(dead);
+	}
+
+	public void die() {
+		Logger.startFunction("die");
+		dead = true;
+		Logger.endFunction();
+	}
+
+	public Tile getTile() {
+		Logger.startFunction("getTile");
+		return Logger.endFunction(tile);
+	}
+
+	public void leaveTile() {
+		Logger.startFunction("leaveTile");
+		tile.setAnimal(this);
+		Logger.endFunction();
+	}
+
+	public void replaceTile(Tile tile) {
+		Logger.startFunction("replaceTile", tile);
+		leaveTile();
+		tile.setAnimal(this);
+		Logger.endFunction();
+	}
+
+	public void move(Tile tile) {
+		Logger.startFunction("move", tile);
+		Tile prev = this.tile;
+		if (tile.stepOn(this)) followedBy.move(prev);
+		Logger.endFunction();
+	}
+
+	public boolean follow(Animal animal) {
+		Logger.startFunction("follow", animal);
+		return Logger.endFunction(false);
+	}
+
+	public void unfollow() {
+		Logger.startFunction("unfollow");
+		Logger.endFunction();
+	}
+
+	public void release() {
+		Logger.startFunction("release");
+		Logger.endFunction();
+	}
+
+	public void hearBeeping() {
+		Logger.startFunction("hearBeeping");
+		Logger.endFunction();
+	}
+
+	public void hearRinging() {
+		Logger.startFunction("hearRinging");
+		Logger.endFunction();
+	}
+
+	public void exit() {
+		Logger.startFunction("exit");
+		Logger.endFunction();
+	}
 }
--- src/hu/bme/iit/beta/pandaexpress/model/tile/Tile.java
+++ src/hu/bme/iit/beta/pandaexpress/model/tile/Tile.java
@@ -1,4 +1,55 @@
 package hu.bme.iit.beta.pandaexpress.model.tile;
 
+import hu.bme.iit.beta.pandaexpress.debug.Logger;
+import hu.bme.iit.beta.pandaexpress.model.animal.Animal;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Tile {
+
+	private List<Tile> neighbors = new LinkedList<>();
+	private Animal animal = null;
+
+	public Tile() {
+		Logger.startFunction("Tile");
+		Logger.endFunction();
+	}
+
+	public Tile[] getNeighbors() {
+		Logger.startFunction("getNeighbors");
+		return Logger.endFunction(neighbors.toArray(new Tile[0]));
+	}
+
+	public void connectNeighbor(Tile neighbor) {
+		Logger.startFunction("connectNeighbor", neighbor);
+		neighbors.add(neighbor);
+		Logger.endFunction();
+	}
+
+	public Animal getAnimal() {
+		Logger.startFunction("getAnimal");
+		return Logger.endFunction(animal);
+	}
+
+	public void setAnimal(Animal animal) {
+		Logger.startFunction("setAnimal", animal);
+		this.animal = animal;
+		Logger.endFunction();
+	}
+
+	public boolean stepOn(Animal animal) {
+		Logger.startFunction("stepOn", animal);
+		if (this.animal == null) {
+			animal.replaceTile(this);
+			return Logger.endFunction(true);
+		} else {
+			return Logger.endFunction(false);
+		}
+	}
+
+	public boolean sitOn(Animal animal) {
+		Logger.startFunction("sitOn", animal);
+		return Logger.endFunction(false);
+	}
 }
