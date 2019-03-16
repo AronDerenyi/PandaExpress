7b68456b08357e8424d6dd31dc383710ecb9acb4
AronDerenyi <aron.derenyi@gmail.com>
2019-03-16 19:22:40 +0100

    Created Entry tile and added global entry to the Stage

--- src/hu/bme/iit/beta/pandaexpress/model/Stage.java
+++ src/hu/bme/iit/beta/pandaexpress/model/Stage.java
@@ -1,4 +1,20 @@
 package hu.bme.iit.beta.pandaexpress.model;
 
+import hu.bme.iit.beta.pandaexpress.debug.Logger;
+import hu.bme.iit.beta.pandaexpress.model.tile.Entry;
+
 public class Stage {
+
+	private static Entry entry = null;
+
+	public static Entry getEntry() {
+		Logger.startFunction("getEntry");
+		return Logger.endFunction(entry);
+	}
+
+	public static void setEntry(Entry entry) {
+		Logger.startFunction("setEntry", entry);
+		Stage.entry = entry;
+		Logger.endFunction();
+	}
 }
