27dc509ea3a99a8bccd07729234308374152f152
AronDerenyi <aron.derenyi@gmail.com>
2019-03-16 20:13:46 +0100

    Created the panda class

--- src/hu/bme/iit/beta/pandaexpress/model/animal/panda/Panda.java
+++ src/hu/bme/iit/beta/pandaexpress/model/animal/panda/Panda.java
@@ -1,4 +1,61 @@
 package hu.bme.iit.beta.pandaexpress.model.animal.panda;
 
-public class Panda {
+import hu.bme.iit.beta.pandaexpress.debug.Logger;
+import hu.bme.iit.beta.pandaexpress.model.Stage;
+import hu.bme.iit.beta.pandaexpress.model.Steppable;
+import hu.bme.iit.beta.pandaexpress.model.animal.Animal;
+import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
+
+public class Panda extends Animal implements Steppable {
+
+	private boolean exiting = false;
+
+	public Panda(Tile tile) {
+		Logger.startFunction("Panda", tile);
+		move(tile);
+		Logger.endFunction();
+	}
+
+	public void exit() {
+		Logger.startFunction("exit");
+
+		// Only exit if it's following someone
+		if (getFollowing() != null) {
+			exiting = true;
+		}
+
+		Logger.endFunction();
+	}
+
+	@Override
+	public void step() {
+		Logger.startFunction("step");
+
+		if (exiting) {
+			Stage.score(1);
+
+			Tile tile = getTile();
+			Animal followedBy = getFollowedBy();
+
+			leaveTile();
+			if (followedBy != null) {
+				followedBy.move(tile);
+				followedBy.unfollow();
+			}
+
+			die();
+		} else if (!isDead() && getFollowing() == null) {
+			// Step randomly on 10% of every step
+			if (Math.random() < 0.1) {
+				Tile tile = getTile();
+				if (tile != null) {
+					Tile[] neighbors = tile.getNeighbors();
+					int index = (int) (neighbors.length * Math.random());
+					move(neighbors[index]);
+				}
+			}
+		}
+
+		Logger.endFunction();
+	}
 }
