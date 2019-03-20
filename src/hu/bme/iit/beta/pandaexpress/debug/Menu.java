22df16c3d4e41b79d9aee78b59c93da65eba8e07
Adam Balassa <balassaadi@gmail.com>
2019-03-20 21:32:19 +0100

    slotMachine rings

--- src/hu/bme/iit/beta/pandaexpress/debug/Menu.java
+++ src/hu/bme/iit/beta/pandaexpress/debug/Menu.java
@@ -3,7 +3,9 @@ package hu.bme.iit.beta.pandaexpress.debug;
 import hu.bme.iit.beta.pandaexpress.model.animal.Orangutan;
 import hu.bme.iit.beta.pandaexpress.model.animal.panda.BeepingPanda;
 import hu.bme.iit.beta.pandaexpress.model.animal.panda.Panda;
+import hu.bme.iit.beta.pandaexpress.model.animal.panda.RingingPanda;
 import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
+import hu.bme.iit.beta.pandaexpress.model.tile.machine.SlotMachine;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -18,6 +20,8 @@ public class Menu {
             System.out.println("\t1: Panda steps");
             System.out.println("\t2: ");
             System.out.println("\t3: Panda follows orangutan");
+            System.out.println("\t4: ");
+            System.out.println("\t5: Slot machine rings");
 
             Scanner reader = new Scanner(System.in);
             int menuItem = reader.nextInt();
@@ -39,6 +43,9 @@ public class Menu {
             case 3:
                 pandaFollowsOrangutan();
                 break;
+            case 5:
+                slotMachineRings();
+                break;
             default:
                 System.out.println("The selected number isn't a menu item");
                 return;
@@ -50,9 +57,12 @@ public class Menu {
 
         //init
 	    Logger.disable();
+
         Tile tileUnderPanda = Logger.addAlias(new Tile(), "tileUnderPanda");
         Tile tileWherePandaSteps = Logger.addAlias(new Tile(), "tileWherePandaSteps");
+
 	    Panda panda = Logger.addAlias(new BeepingPanda(tileUnderPanda), "Panda");
+
 	    Logger.enable();
 
 	    tileUnderPanda.connectNeighbor(tileWherePandaSteps);
@@ -66,11 +76,14 @@ public class Menu {
 
         //init
         Logger.disable();
+
         Tile tileUnderOrangutan = Logger.addAlias(new Tile(), "tileUnderOrangutan");
         Tile tileUnderPanda = Logger.addAlias(new Tile(), "tileUnderPanda");
         Tile tileWhereOrangutanSteps = Logger.addAlias(new Tile(), "tileWhereOrangutanSteps");
+
         Panda panda = Logger.addAlias(new BeepingPanda(tileUnderPanda), "Panda");
         Orangutan orangutan = Logger.addAlias(new Orangutan(tileUnderOrangutan), "Orangutan");
+
         Logger.enable();
 
         tileUnderOrangutan.connectNeighbor(tileUnderPanda);
@@ -80,5 +93,31 @@ public class Menu {
 
         //run
         orangutan.move(tileWhereOrangutanSteps);
+    }
+
+    private void slotMachineRings(){
+        System.out.println("Slot machine rings:");
+
+        //init
+        Logger.disable();
+
+        Tile tileUnderPanda1 = Logger.addAlias(new Tile(), "tileUnderPanda1");
+        Tile tileUnderPanda2 = Logger.addAlias(new Tile(), "tileUnderPanda2");
+
+        int[] slotMachineConfig = {0};
+        SlotMachine slotMachine = Logger.addAlias(new SlotMachine(slotMachineConfig), "slotMachine");
+
+        Panda panda1 = Logger.addAlias(new RingingPanda(tileUnderPanda1), "RingingPanda");
+        Panda panda2 = Logger.addAlias(new BeepingPanda(tileUnderPanda2), "FollowingPanda");
+
+        Logger.enable();
+
+        tileUnderPanda1.connectNeighbor(tileUnderPanda2);
+        tileUnderPanda1.connectNeighbor(slotMachine);
+
+        panda2.follow(panda1);
+
+        //run
+        slotMachine.step();
     }
 }
