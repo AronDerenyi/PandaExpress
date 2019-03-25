93b0b18a83da281209e33314f91d089ffe8e3ac2
AronDerenyi <aron.derenyi@gmail.com>
2019-03-25 11:35:10 +0100

    PeepingPanda documentation

--- src/hu/bme/iit/beta/pandaexpress/model/animal/panda/BeepingPanda.java
+++ src/hu/bme/iit/beta/pandaexpress/model/animal/panda/BeepingPanda.java
@@ -5,12 +5,24 @@ import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
 
 public class BeepingPanda extends Panda {
 
+	/**
+	 * The BeepingPanda's constructor which requires an
+	 * initial tile for the panda to move to.
+	 *
+	 * @param tile The panda's initial tile
+	 */
 	public BeepingPanda(Tile tile) {
 		super(tile);
 		Logger.startFunction(this, "BeepingPanda", tile);
 		Logger.endFunction();
 	}
 
+	/**
+	 * Overrides the animal's hearBeeping function (for further information
+	 * read the documentation of the animal's hearBeeping method).
+	 *
+	 * Steps on the current tile again. This will make a weak tile lose lives.
+	 */
 	@Override
 	public void hearBeeping() {
 		Logger.startFunction(this, "hearBeeping");
