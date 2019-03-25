d0a54c3fb8e56e5e83354bcffa8c3c90c5c34714
AronDerenyi <aron.derenyi@gmail.com>
2019-03-25 11:33:11 +0100

    RingingPanda documentation

--- src/hu/bme/iit/beta/pandaexpress/model/animal/panda/RingingPanda.java
+++ src/hu/bme/iit/beta/pandaexpress/model/animal/panda/RingingPanda.java
@@ -5,12 +5,24 @@ import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
 
 public class RingingPanda extends Panda {
 
+	/**
+	 * The RingingPanda's constructor which requires an
+	 * initial tile for the panda to move to.
+	 *
+	 * @param tile The panda's initial tile
+	 */
 	public RingingPanda(Tile tile) {
 		super(tile);
 		Logger.startFunction(this, "RingingPanda", tile);
 		Logger.endFunction();
 	}
 
+	/**
+	 * Overrides the animal's hearRinging function (for further information
+	 * read the documentation of the animal's hearRinging method).
+	 *
+	 * Unfollows and releases all of it's related animals.
+	 */
 	@Override
 	public void hearRinging() {
 		Logger.startFunction(this, "hearRinging");
