297e8bfdc46d92f4261246f7343e62830612f7ba
pp <pp@152.66.237.223>
2019-04-24 00:48:57 +0200

    Wardrobe javítás

--- src/hu/bme/iit/beta/pandaexpress/model/tile/Wardrobe.java
+++ src/hu/bme/iit/beta/pandaexpress/model/tile/Wardrobe.java
@@ -1,6 +1,6 @@
 package hu.bme.iit.beta.pandaexpress.model.tile;
 
-import hu.bme.iit.beta.pandaexpress.debug.Logger;
+
 import hu.bme.iit.beta.pandaexpress.model.animal.Animal;
 
 public class Wardrobe extends Tile {
@@ -10,32 +10,34 @@ public class Wardrobe extends Tile {
 	
 	// This method sets the entry of a wardrobe, and connects them
     public void setEntry(Tile entry) {
-		Logger.startFunction(this, "setEntry", entry);
 		this.entry= entry;
 		entry.connectNeighbor(this);
-		Logger.endFunction();
 	}
+    
+    public Tile getEntry() {
+    	return entry;
+    }
 	
     // connects two Wardrobe
 	public void connect(Wardrobe pair) {
-		Logger.startFunction(this, "connect", pair);
 		this.pair= pair;
 		this.pair.pair = this;
-		Logger.endFunction();
+	}
+	
+	public Wardrobe getPair() {
+		return pair;
 	}
 	
 	// Tile stepOn override - The Wardrobe teleports the animal who on it 
 	// to the other Wardrobe's entry
 	@Override
 	public boolean stepOn(Animal a) {
-		Logger.startFunction(this, "stepOn", a);
-		return Logger.endFunction(pair.entry.stepOn(a));
+		return pair.entry.stepOn(a);
 	}
 	
 	// Tile getAnimal override - returns the animal who is standing on the Wardrobe's pair's entry
 	@Override
 	public Animal getAnimal() {
-		Logger.startFunction(this, "getAnimal");
-		return Logger.endFunction(pair.entry.getAnimal());
+		return pair.entry.getAnimal();
 	}
 }
