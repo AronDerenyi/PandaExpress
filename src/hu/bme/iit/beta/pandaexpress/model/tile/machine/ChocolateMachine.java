3a478b51e3329782c03191d943cd7b81500e5896
pp <pp@152.66.237.223>
2019-04-24 00:48:33 +0200

    ChocolateMachine javítás

--- src/hu/bme/iit/beta/pandaexpress/model/tile/machine/ChocolateMachine.java
+++ src/hu/bme/iit/beta/pandaexpress/model/tile/machine/ChocolateMachine.java
@@ -1,6 +1,5 @@
 package hu.bme.iit.beta.pandaexpress.model.tile.machine;
 
-import hu.bme.iit.beta.pandaexpress.debug.Logger;
 import hu.bme.iit.beta.pandaexpress.model.animal.Animal;
 import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
 
@@ -9,15 +8,12 @@ public class ChocolateMachine extends Machine {
 	// Tile stepOn override - return false, because animals can't step on a chocolate machine
 	@Override
 	public boolean stepOn(Animal a) {
-		Logger.startFunction(this, "stepOn", a);
-		return Logger.endFunction(false);
+		return false;
 	}
 
 	// Chocolate machine beeps
 	@Override
 	protected void makeNoise() {
-		Logger.startFunction(this, "makeNoise");
-		
 		Tile[] neighbors= getNeighbors();
 		for(Tile neighbor: neighbors){
 			Animal animalOnNeighbor = neighbor.getAnimal();
@@ -25,7 +21,6 @@ public class ChocolateMachine extends Machine {
 				animalOnNeighbor.hearBeeping();
 		}
 		
-		Logger.endFunction();
 	}
 
 }
