5c1a761517e5cdf24052a8c3dedadd462f522afb
Adam Balassa <balassaadi@gmail.com>
2019-05-13 10:31:18 +0200

    game under construction

--- src/hu/bme/iit/beta/pandaexpress/controller/Controller.java
+++ src/hu/bme/iit/beta/pandaexpress/controller/Controller.java
@@ -1,14 +1,23 @@
 package hu.bme.iit.beta.pandaexpress.controller;
 
 import hu.bme.iit.beta.pandaexpress.controller.view.View;
+import hu.bme.iit.beta.pandaexpress.controller.view.animalview.OrangutanView;
+import hu.bme.iit.beta.pandaexpress.controller.view.animalview.pandaview.BeepingPandaView;
+import hu.bme.iit.beta.pandaexpress.controller.view.animalview.pandaview.RingingPandaView;
+import hu.bme.iit.beta.pandaexpress.controller.view.animalview.pandaview.TiredPandaView;
+import hu.bme.iit.beta.pandaexpress.controller.view.tileview.*;
 import hu.bme.iit.beta.pandaexpress.debug.interpreter.Environment;
 import hu.bme.iit.beta.pandaexpress.debug.interpreter.Interpreter;
 import hu.bme.iit.beta.pandaexpress.model.Stage;
 import hu.bme.iit.beta.pandaexpress.model.Steppable;
 import hu.bme.iit.beta.pandaexpress.model.animal.Animal;
 import hu.bme.iit.beta.pandaexpress.model.animal.Orangutan;
-import hu.bme.iit.beta.pandaexpress.model.tile.Tile;
-import hu.bme.iit.beta.pandaexpress.model.tile.WeakTile;
+import hu.bme.iit.beta.pandaexpress.model.animal.panda.BeepingPanda;
+import hu.bme.iit.beta.pandaexpress.model.animal.panda.RingingPanda;
+import hu.bme.iit.beta.pandaexpress.model.animal.panda.TiredPanda;
+import hu.bme.iit.beta.pandaexpress.model.tile.*;
+import hu.bme.iit.beta.pandaexpress.model.tile.machine.ChocolateMachine;
+import hu.bme.iit.beta.pandaexpress.model.tile.machine.SlotMachine;
 import hu.bme.iit.beta.pandaexpress.window.Screen;
 
 import java.awt.*;
@@ -32,8 +41,11 @@ public class Controller extends Screen implements Steppable {
 
 	private Orangutan selectedOrangutan;
 	private List<View> views;
+	private Timer timer;
 
 	private Controller(){
+		timer = new Timer();
+		timer.addSteppable(this);
 		views = new ArrayList<>();
 		try {
 			Interpreter interpreter = new Interpreter(
@@ -47,6 +59,7 @@ public class Controller extends Screen implements Steppable {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		timer.start();
 	}
 
 	private static void DFS(Tile root, Set<Tile> foundTiles){
@@ -59,67 +72,73 @@ public class Controller extends Screen implements Steppable {
 	private void createViews(Collection<Tile> tiles){
 		for(Tile tile : tiles){
 			views.add(createTileView(tile));
+			registerIfSteppable(tile);
+		}
+		for(Tile tile : tiles){
 			Animal animal;
 			if((animal = tile.getAnimal()) != null)
 				views.add(createAnimalView(animal));
+			registerIfSteppable(animal);
 		}
 	}
 
 	private View createTileView(Tile tile){
-//		Class tileType = tile.getClass();
-//		if(tileType == WeakTile.class)
-//			return new WeakTileView(tile);
-		return new View() {
-			@Override
-			public void onDraw(Graphics2D graphics, int screenWidth, int screenHeight) {
-				drawCircle(graphics, 100, 100, 16, Color.CYAN, "T1");
-			}
-		};
+		Class tileType = tile.getClass();
+		if(tileType == Chair.class)
+			return new ChairView(tile);
+		if(tileType == ChocolateMachine.class)
+			return new ChocolateMachineView(tile);
+		if(tileType == Entry.class)
+			return new EntryView(tile);
+		if(tileType == Exit.class)
+			return new ExitView(tile);
+		if(tileType == SlotMachine.class)
+			return new SlotMachineView(tile);
+		if(tileType == Tile.class)
+			return new TileView(tile);
+		if(tileType == Wardrobe.class)
+			return new WardrobeView(tile);
+		if(tileType == WeakTile.class)
+			return new WeakTileView(tile);
+		return null;
 	}
 
 	private View createAnimalView(Animal animal){
-//		Class animalType = animal.getClass();
-//		if(animalType == Orangutan.class)
-//			return new WeakTileView(tile);
-		return new View() {
-			@Override
-			public void onDraw(Graphics2D graphics, int screenWidth, int screenHeight) {
-				drawCircle(graphics, 100, 100, 16, Color.CYAN, "T1");
-			}
-		};
+		Class animalType = animal.getClass();
+		if(animalType == Orangutan.class)
+			return new OrangutanView(animal);
+		if(animalType == BeepingPanda.class)
+			return new BeepingPandaView(animal);
+		if(animalType == RingingPanda.class)
+			return new RingingPandaView(animal);
+		if(animalType == TiredPanda.class)
+			return new TiredPandaView(animal);
+		return null;
 	}
+
+	private void registerIfSteppable(Object o){
+		if(o instanceof Steppable)
+			timer.addSteppable((Steppable)o);
+	}
+
 	@Override
 	public void onAttach() {
 		Graphics2D g = getGraphics();
 		g.setPaint(Color.WHITE);
 		g.fillRect(0, 0, getWidth(), getHeight());
 
-		View v1 = new View() {
-			@Override
-			public void onDraw(Graphics2D graphics, int screenWidth, int screenHeight) {
-				drawCircle(graphics, 100, 100, 16, Color.CYAN, "T1");
-			}
-		};
-
-		View v2 = new View() {
-			@Override
-			public void onDraw(Graphics2D graphics, int screenWidth, int screenHeight) {
-				drawCircle(graphics, 300, 200, 16, Color.YELLOW, "T2");
-				drawConnection(graphics, 300, 200, 16, 100, 100, 16);
-			}
-		};
-
-		v1.onDraw(g, getWidth(), getHeight());
-		v2.onDraw(g, getWidth(), getHeight());
+		step();
 
 		flush();
 	}
 
 	public void setSelectedOrangutan(Orangutan selected){
 		selectedOrangutan = selected;
+		System.out.println("orangutan selected");
 	}
 
 	public void tileClicked(Tile tile){
+		if(selectedOrangutan == null) return;
 		for(Tile t: selectedOrangutan.getTile().getNeighbors())
 			if(t == tile){
 				selectedOrangutan.move(tile);
@@ -140,7 +159,9 @@ public class Controller extends Screen implements Steppable {
 		Graphics2D g = getGraphics();
 		g.setPaint(Color.WHITE);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		for(View v: views) v.onDraw(g, getWidth(), getHeight());
+		for(View v: views) {
+			v.onDraw(g, getWidth(), getHeight());
+		}
 		flush();
 	}
 }
--- src/hu/bme/iit/beta/pandaexpress/controller/view/animalview/OrangutanView.java
+++ src/hu/bme/iit/beta/pandaexpress/controller/view/animalview/OrangutanView.java
@@ -1,6 +1,8 @@
 package hu.bme.iit.beta.pandaexpress.controller.view.animalview;
 
+import hu.bme.iit.beta.pandaexpress.controller.Controller;
 import hu.bme.iit.beta.pandaexpress.model.animal.Animal;
+import hu.bme.iit.beta.pandaexpress.model.animal.Orangutan;
 
 import java.awt.*;
 
@@ -15,5 +17,15 @@ public class OrangutanView extends AnimalView {
         int tileX = ref.getTile().getPositionX();
         int tileY = ref.getTile().getPositionY();
         drawCircle(graphics, tileX - offset, tileY - offset, r, Color.GREEN);
+    }
+
+    @Override
+    public boolean onClick(int mouseX, int mouseY) {
+        int tileX = ref.getTile().getPositionX();
+        int tileY = ref.getTile().getPositionY();
+        if(!isCircleClicked(mouseX, mouseY, tileX - offset, tileY - offset, r))
+            return false;
+        Controller.getInstance().setSelectedOrangutan((Orangutan) ref);
+        return true;
     }
 }
